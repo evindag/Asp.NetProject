0ab96243306eea24369cc1366204e3473ccdf1c9
Evin <80323753+evindag@users.noreply.github.com>
2025-05-06 11:32:25 +0300

    commit

--- MVCProject/Controllers/KitapController.cs
+++ MVCProject/Controllers/KitapController.cs
@@ -3,7 +3,16 @@ public class KitapController: Controller
 {
  public IActionResult Kategoriler()
  {
-    return View();
+    var kitap = new KitapModel{
+      KitapId = 1,
+      KitapAdi="WTUG",
+      SayfaSayisi = 321
+    };
+    ViewBag.Kitap=kitap; 
+    //ViewBag.Kitap ile "kitap" modeli view'e aktarılıyor
+    ViewBag.Sahip ="Milli Eğitim Bakanlığı";
+    // ViewBag.Sahip ile bir string veri view'e aktarılıyor
+    return View(); //View sayfası çağırılıyor
  }
 
  public IActionResult KitapListesi(int id)
---
+++ MVCProject/Models/KitapModel.cs
@@ -0,0 +1,5 @@
+public class KitapModel{
+public int KitapId { get; set; }
+public string KitapAdi { get; set; }
+public int SayfaSayisi { get; set; }
+}
